54a5fc1c83537a47c4d0b472bef617ea5ba1b100
ArAmAn <artem.amosov@mail.ru>
2018-03-31 23:53:49 +0300

    Heroku setup

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 import telebot
 import config
-import requests
 import vk
 import random
-from voice import say, recognize
-import os
-import time
-import aneks
 
 bot = telebot.TeleBot(config.token_telegram)
 
@@ -29,46 +24,6 @@ def help(message):
 def start(message):
     bot.send_message(message.chat.id, '/say - рандомный (почти) анек голосом')
 
-@bot.message_handler(commands=['upload'])
-def upload(message):
-    if message.chat.id == config.admin_chat:
-        try:
-            n = int(message.text[8:])
-        except:
-            bot.send_message(message.chat.id, 'enter number')
-            return
-        anekdots = aneks.get(n)
-
-        number = 0
-        errors = 0
-        start = 3844
-        for i in range(start, len(anekdots)):
-            try:
-                token = config.token_yandex[number]
-                errors = 0
-                files = open('ids.txt', 'a')
-                file = say(anekdots[i], token)
-                audio = open(file, 'rb')
-                msg = bot.send_audio(message.chat.id, audio)
-                print(msg.audio.file_id, file=files)
-                files.close()
-                path = os.path.join(os.path.abspath(os.path.dirname(__file__)), file)
-                os.remove(path)
-                print(i, 'ready')
-                time.sleep(1)
-            except:
-                bot.send_message(message.chat.id, 'SOME STRANGE ERROR' + str(i))
-                errors += 1
-                if errors >= 5:
-                    bot.send_message(message.chat.id, 'OOOps, lots of errors, using the next token and sleeping 20 secs')
-                    if number >= len(config.token_yandex):
-                        bot.send_message(message.chat.id, 'no more tokens((')
-                        files.close()
-                        return
-                    number += 1
-                    time.sleep(20)
-    else:
-        bot.send_message(message.chat.id, 'You-re not admin')
 
 
 if __name__ == '__main__':
